1b899e7e0cf026c7604b7abfe548f928f77e1040
kim jaewon <201901315a@inu.ac.kr>
2025-12-16 15:02:35 +0900

    first commit

---
+++ src/main/java/com/example/IncheonMate/IncheonMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.IncheonMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncheonMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncheonMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IncheonMate/IncheonMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IncheonMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncheonMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
