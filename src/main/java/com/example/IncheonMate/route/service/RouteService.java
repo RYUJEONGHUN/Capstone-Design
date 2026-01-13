6a66ef0578c11de14561097f2259d111f5352fff
김재원 <wodnjs9058@naver.com>
2026-01-13 23:19:03 +0900

    feat: 카카오 맵 키워드 검색 및 저장 service logic 추가

--- src/main/java/com/example/IncheonMate/route/service/RouteService.java
+++ src/main/java/com/example/IncheonMate/route/service/RouteService.java
@@ -4,6 +4,10 @@ import com.example.IncheonMate.common.exception.CustomException;
 import com.example.IncheonMate.common.exception.ErrorCode;
 import com.example.IncheonMate.member.domain.Member;
 import com.example.IncheonMate.member.repository.MemberRepository;
+import com.example.IncheonMate.place.client.KakaoFeignClient;
+import com.example.IncheonMate.place.domain.Place;
+import com.example.IncheonMate.place.dto.KakaoApiResponseDto;
+import com.example.IncheonMate.place.repository.PlaceRepository;
 import com.example.IncheonMate.route.client.OdsayClient;
 import com.example.IncheonMate.route.dto.OdsayRouteSearchResponse;
 import com.example.IncheonMate.route.dto.RouteRequest;
@@ -21,8 +25,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
@@ -33,19 +41,14 @@ public class RouteService {
     private final OdsayClient odsayClient;
     private final MemberRepository memberRepository;
     private final MongoTemplate mongoTemplate;//기록 개수 제한을 위해서 MongoTempalte를 사용
+    private final KakaoFeignClient kakaoFeignClient;
+    private final PlaceRepository placeRepository;
 
     @Value("${ODSAY_KEY}")
     private String apiKey;
 
-//    public Map<String, Object> getRoute(){
-//        log.info("요청함");
-//        Map<String, Object> response = odsayClient.searchRoute(
-//                "126.9027279","37.5349277",
-//                "126.9145430","37.5499421",
-//                apiKey);
-//
-//        return response;
-//    }
+    @Value("${kakao.api.key}")
+    private String kakaoKey;
 
     //1.길찾기 초기화면: 최근 길찾기 기록을 반환하는 메소드(/api/route/history/paths)
     public List<RouteResponse.RecentRouteDto> getRecentRoutes(String email) {
@@ -88,28 +91,56 @@ public class RouteService {
     public List<RouteResponse.CurrentPlaceDto> searchAndSavePlaces(String email, String keyword, boolean save) {
         log.debug("email:{}, keyword: {}, save:{}", email, keyword, save);
 
-        //요청한다음에 KakaoMapKeywordSearchResponse로 전체 카카오 응답 받기(여기서 keyword 사용)
-        //KakaoMapKeywordSearchResponse kakaoResponse = xxxxxxxxService.xxxxxxxxxx(keyword);
-        //if(save) {//save=true이면 저장하고 전송
-        //log.info("장소 검색 결과: {}",kakaoRespnse.getxxxx.getxxxxx.size());
-        //Member.RecentSearch recentSearch = Member.RecentSearch.builder().id(UUID.randomUUID().toString()).title().type().searchedAt(LocalDateTime.now());
-        //저장 로직MongoTemplate(최대 20개)
-        //MongoTempalte: 실행 엔진 계층으로 직접적으로 MongoDB에 BSON을 전송
-        //Query query = new Query(Criteria.where("email").is(email));
+        //요청한다음에 KakaoApiResponseDto 전체 카카오 응답 받기(여기서 keyword 사용)
+        KakaoApiResponseDto kakaoApiResponseDto = kakaoFeignClient.searchByKeyword("KakaoAK " + kakaoKey, keyword);
+        log.info("장소 검색 결과: {}개", kakaoApiResponseDto.getMeta().getPageableCount());
 
-        //Update update = new Update().push("recentPlaces") //Member 엔티티의 필드명
-//                .atPosition(0) //0번 인덱스에 삽입(최신순)
-//                .slice(20) //배열의 크기를 20개로 유지
-//                .each(recentSearch); //삽입할 데이터
-//
-//        mongoTemplate.updateFirst(query,update,Member.class);
-        //log.info("장소 검색 결과 저장 완료: {}" ,email);
-        //return RouteResponse.CurrentPlaceDto.from(kakaoResponse.document());
-        //}
+        //우리 DB에 저장된 장소이면 isRegistered를 true로 바꾼후 프론트에 전달할 형식으로 변경
+        //1. 카카오 응답에서 ID만 추출
+        List<String> kakaoPlaceIds = kakaoApiResponseDto.getDocuments().stream()
+                .map(KakaoApiResponseDto.DocumentDto::getId)
+                .toList();
+        //2. DB에 존재하는 ID들을 한번에 조회
+        Set<String> registeredIds = placeRepository.findAllByKakaoIdIn(kakaoPlaceIds).stream()
+                .map(Place::getKakaoId)
+                .collect(Collectors.toSet());
+        //3. 비교하여 ture/flase list 생성
+        List<Boolean> isRegistereds = kakaoPlaceIds.stream()
+                .map(registeredIds::contains)
+                .toList();
+        //return 해줄 List dto 생성
+        List<RouteResponse.CurrentPlaceDto> result = IntStream.range(0, kakaoApiResponseDto.getDocuments().size())
+                .mapToObj(i -> RouteResponse.CurrentPlaceDto.from(
+                        kakaoApiResponseDto.getDocuments().get(i),
+                        isRegistereds.get(i)
+                ))
+                .toList();
+
+
+        if (save) {//save=true이면 저장하고 전송
+            //저장할 recentSearch 생성
+            Member.RecentSearch recentSearch = Member.RecentSearch.builder()
+                    .id(UUID.randomUUID().toString())
+                    .keyword(keyword)
+                    .searchedAt(LocalDateTime.now())
+                    .build();
+            //최대 20개만 저장하는 로직
+            //MongoTempalte: 실행 엔진 계층으로 직접적으로 MongoDB에 BSON을 전송
+            Query query = new Query(Criteria.where("email").is(email));
+            Update update = new Update().push("recentSearches") //Member 엔티티의 필드명
+                    .atPosition(0) //0번 인덱스에 삽입(최신순)
+                    .slice(20) //배열의 크기를 20개로 유지
+                    .each(recentSearch); //삽입할 데이터
+            UpdateResult resultCount = mongoTemplate.updateFirst(query, update, Member.class);
+            if (resultCount.getMatchedCount() == 0) {
+                log.error("사용자를 찾을 수 없습니다.: {}", email);
+                throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
+            }
+            log.info("장소 검색 결과 저장 완료: {}", email);
+            return result;
+        }
         //save=false이면 저장하지 않고 dto로 전송
-        //return RouteResponse.CurrentPlaceDto.from(kakaoResponse.document());
-
-        return null;
+        return result;
     }
 
     //4.길찾기 조회 완료 화면: 출발지와 목적지를 입력하고 '길찾기'를 누르면 그에 맞는 경로들을 보여주면서 저장하는 기능-POST/findAndSavePaths(/api/route/paths)
@@ -121,11 +152,11 @@ public class RouteService {
                 routeSearchRequest.sx(), routeSearchRequest.sy(),
                 routeSearchRequest.ex(), routeSearchRequest.ey(), apiKey);
         //비정상 흐름 처리
-        if(odsayResponse.error() != null){
-            log.warn("ODsay 길찾기 에러: {},에러코드={}",email,odsayResponse.error().code());
+        if (odsayResponse.error() != null) {
+            log.warn("ODsay 길찾기 에러: {},에러코드={}", email, odsayResponse.error().code());
             handleOdsayError(odsayResponse.error().code());
         }
-        if(odsayResponse.result() == null || odsayResponse.result().path() == null){
+        if (odsayResponse.result() == null || odsayResponse.result().path() == null) {
             log.info("ODsay 경로 탐색 결과 없음 (path is null)");
             throw new CustomException(ErrorCode.NO_SEARCH_RESULT);
         }
@@ -155,8 +186,8 @@ public class RouteService {
                 .each(recentRoute); //삽입할 데이터
 
         UpdateResult result = mongoTemplate.updateFirst(query, update, Member.class);
-        if(result.getMatchedCount() == 0){
-            log.error("사용자를 찾을 수 없습니다.: {}",email);
+        if (result.getMatchedCount() == 0) {
+            log.error("사용자를 찾을 수 없습니다.: {}", email);
             throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
         }
         log.info("경로 기록 저장 완료: {}", email);
@@ -171,15 +202,15 @@ public class RouteService {
     }
 
     // ODsay 에러 코드 핸들링 내부 method
-    private void handleOdsayError(String odsayErrorCode){
-        throw switch (odsayErrorCode){
+    private void handleOdsayError(String odsayErrorCode) {
+        throw switch (odsayErrorCode) {
             case "500" -> new CustomException(ErrorCode.ODSAY_SERVER_ERROR);
-            case "-8","-9" -> new CustomException(ErrorCode.INVALID_INPUT_VALUE);
-            case "3","4","5" -> new CustomException(ErrorCode.STATION_NOT_FOUND);
+            case "-8", "-9" -> new CustomException(ErrorCode.INVALID_INPUT_VALUE);
+            case "3", "4", "5" -> new CustomException(ErrorCode.STATION_NOT_FOUND);
             case "6" -> new CustomException(ErrorCode.NOT_A_SERVICE_AREA);
             case "-98" -> new CustomException(ErrorCode.DISTANCE_TOO_SHORT);
             case "-99" -> new CustomException(ErrorCode.NO_SEARCH_RESULT);
-            default -> new CustomException(ErrorCode.INTERNAL_SERVER_ERROR,"Odsay 요청 중 예상하지 못한 예외 발생");
+            default -> new CustomException(ErrorCode.INTERNAL_SERVER_ERROR, "Odsay 요청 중 예상하지 못한 예외 발생");
         };
     }
 }
