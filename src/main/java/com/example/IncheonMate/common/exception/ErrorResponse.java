5acb9f7d8b6e1fb217d57ef829ddf5594f91ca60
김재원 <wodnjs9058@naver.com>
2026-01-16 14:16:11 +0900

    docs: auth 컨트롤러 및 ErrorResponse 문서화

--- src/main/java/com/example/IncheonMate/common/exception/ErrorResponse.java
+++ src/main/java/com/example/IncheonMate/common/exception/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.example.IncheonMate.common.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import org.springframework.http.ResponseEntity;
 
@@ -7,9 +8,13 @@ import java.time.LocalDateTime;
 
 @Builder
 //모든 에러를 에러코드,에러메시지,에러발생시간으로 일관성 있게 응답하기 위해 만든 DTO
+@Schema(description = "모든 에러를 에러코드,에러메시지,에러발생시간으로 일관성 있게 응답")
 public record ErrorResponse(
+        @Schema(description = "에러 코드", example = "COMMON 001")
         String code,
+        @Schema(description = "에러 내용", example = "잘못된 입력값입니다.")
         String message,
+        @Schema(description = "에러 발생 시간", example = "2026-01-14T08:37:59.560Z")
         LocalDateTime timestamp
 ) {
     //기본 버전(에러 코드에 정의된 메시지 사용)
