66c4a95a09c88b2453294efaf1a6b2b3d751cfd8
김재원 <wodnjs9058@naver.com>
2026-01-21 12:56:08 +0900

    refactor: Persona 도메인 삭제 후 Member 도메인에서 직접 참조할 수 있도록 Enum으로 대체

--- src/main/java/com/example/IncheonMate/persona/repository/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.IncheonMate.persona.repository;
-
-import com.example.IncheonMate.persona.domain.Persona;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PersonaRepository extends MongoRepository<Persona,String> {
-}
