114bd1a06bf134262aa459409997b8215bebccea
김재원 <wodnjs9058@naver.com>
2026-01-31 20:20:39 +0900

    refactor: 카카오 로그인 방법 SDK 수동 로그인으로 변경

--- src/main/java/com/example/IncheonMate/common/auth/controller/KakaoSdkOauthController.java
+++ src/main/java/com/example/IncheonMate/common/auth/controller/KakaoSdkOauthController.java
@@ -1,11 +1,22 @@
 package com.example.IncheonMate.common.auth.controller;
 
+import com.example.IncheonMate.common.auth.service.KakaoSdkOauthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -13,17 +24,43 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class KakaoSdkOauthController {
 
+    private final KakaoSdkOauthService kakaoSdkOauthService;
+
+    @Value("${app.frontend.redirect-url}")
+    private String redirectUrl;
+
     @Operation(summary = "카카오 SDK 로그인 토큰 발급", description = "카카오 SDK 로그인 로직을 수동으로 진행하여 AccessToken과 RefreshToken을 전송 및 저장합니다.")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Accees Token 전송 및 Refresh Token 저장 완료")
     })
     @GetMapping("/kakao/callback")
-    public void makeTokes(@RequestParam String code){
-        //1. 카카오 토큰 받기
-        //2. 받은 토큰으로 사용자 정보 가져오기
-        //3. 회원가입 또는 로그인 처리
-            //3.1 이미 초기 정보를 입력해서 닉네임이 있는 사용자이면 ROLE_USER로 토큰 발급
-            //3.1 초기 정보를 입력하지 않아서 닉네임이 null인 사용자이면 ROLE_GUEST로 토큰 발급
-        //4. 우리 서비스의 JWT 발급
+    public void makeTokes(@RequestParam String code, HttpServletResponse response) throws IOException {
+        log.info("카카오 SDK 로그인 요청 code: {}", code);
+
+        //로그인 과정은 service에서 하고 Access와 Refresh Token과 role만 가져옴
+        KakaoSdkOauthService.Tokens tokens = kakaoSdkOauthService.kakaoLogin(code);
+
+        //Access Token과 role은 전송하고 Refresh Token은 Cookie에 저장
+        //Refresh Token Cookie에 저장
+        ResponseCookie responseCookie = ResponseCookie.from("refreshToken", tokens.refreshToken())
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(Duration.ofDays(14))
+                .sameSite("None")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, responseCookie.toString());
+
+        //Access Token과 Role을 URI로 전달
+        String targetUri = UriComponentsBuilder.fromUriString(redirectUrl)
+                .queryParam("accessToken", tokens.accessToken())
+                .queryParam("role", tokens.role())
+                .build()
+                .encode(StandardCharsets.UTF_8)
+                .toUriString();
+        log.info("프론트엔드로 리다이렉트-ROLE: {}", tokens.role());
+
+        response.sendRedirect(targetUri);
+
     }
 }
---
+++ src/main/java/com/example/IncheonMate/common/auth/service/KakaoSdkOauthService.java
@@ -0,0 +1,100 @@
+package com.example.IncheonMate.common.auth.service;
+
+import com.example.IncheonMate.common.auth.client.KakaoOauthTokenClient;
+import com.example.IncheonMate.common.auth.client.KakaoOauthUserInfoClient;
+import com.example.IncheonMate.common.auth.dto.KakaoOauthResponse;
+import com.example.IncheonMate.common.exception.CustomException;
+import com.example.IncheonMate.common.exception.ErrorCode;
+import com.example.IncheonMate.common.jwt.JWTUtil;
+import com.example.IncheonMate.member.domain.Member;
+import com.example.IncheonMate.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KakaoSdkOauthService {
+
+    //컨트롤러로 토큰 전송하기 위한 dto
+    public record Tokens(String accessToken, String refreshToken, String role){}
+
+    private final JWTUtil jwtUtil;
+    private final StringRedisTemplate redisTemplate;
+    private final MemberRepository memberRepository;
+    private final KakaoOauthTokenClient kakaoOauthTokenClient;
+    private final KakaoOauthUserInfoClient kakaoOauthUserInfoClient;
+
+    @Value("${KAKAO_CLIENT_ID}")
+    private String clientId;
+
+    @Value("${KAKAO_CLIENT_SECRET}")
+    private String clientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String redirectUri;
+
+    @Transactional
+    public Tokens kakaoLogin(String code) {
+        //1. 코드로 token 받아오기
+        KakaoOauthResponse.TokenResponse tokenResponse = kakaoOauthTokenClient.getTokens(
+                "authorization_code",
+                clientId,
+                redirectUri,
+                code,
+                clientSecret);
+        log.debug("카카오 토큰 받기 성공-Access Token 만료 {}초 남음", tokenResponse.expiresIn());
+
+        //2. token으로 유저 정보 받아오기
+        KakaoOauthResponse.UserInfoResponse userInfoResponse = kakaoOauthUserInfoClient.getInfo("Bearer " + tokenResponse.accessToken());
+        log.debug("카카오에서 유저 정보 받기 성공-성공 시간: {}", userInfoResponse.connectedAt());
+
+        //3. 회원가입 또는 로그인 처리
+        String email = userInfoResponse.kakaoAccount().email();
+        String name = null;
+        //프로필이 null이 아니고 닉네임이 있을때만 name 추출
+        if(userInfoResponse.kakaoAccount().profile() != null){
+            name = userInfoResponse.kakaoAccount().profile().nickname();
+        }
+        //유효성 검사
+        if(!StringUtils.hasText(email) || !StringUtils.hasText(name)){
+            log.error("필수 정보 누락: email={}, nickname={}", email, name);
+            throw new CustomException(ErrorCode.MISSING_REQUIRED_INFO);
+        }
+
+        //3.2 멤버 생성 or 불러오기
+        String finalName = name;
+        Member member = memberRepository.findByEmail(email)
+                .orElseGet(() -> {
+                    log.info("신규 회원 가입 진행: {}", email);
+                    Member newMember = Member.builder()
+                            .email(email)
+                            .name(finalName)
+                            .role("ROLE_GUEST")
+                            .provider("kakao")
+                            .build();
+                    return memberRepository.save(newMember);
+                });
+
+        //4. 우리 서비스의 JWT 발급
+        long accessTime = 60 * 60 * 1000L; // 1시간
+        long refreshTime = 14 * 24 * 60 * 60 * 1000L; // 14일
+
+        String accessToken = jwtUtil.createJwt(email, member.getRole(), accessTime);
+        String refreshToken = jwtUtil.createJwt(email, member.getRole(), refreshTime);
+
+        //5. Refresh Token을 redis에 저장
+        redisTemplate.opsForValue()
+                .set("RT:" + email, refreshToken, 14, TimeUnit.DAYS);
+        log.info("Refresh Token Redis 저장 완료: {}", email);
+
+        return new Tokens(accessToken,refreshToken,member.getRole());
+    }
+}
