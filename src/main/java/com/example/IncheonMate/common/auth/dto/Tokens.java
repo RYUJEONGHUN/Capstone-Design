b7e3db219fb34d01ff305cd7ae91484370c0eb10
김재원 <wodnjs9058@naver.com>
2026-03-11 17:58:05 +0900

    refactor: token dto 변경에 따른 Auth 컨트롤러 수정

--- src/main/java/com/example/IncheonMate/common/auth/dto/Tokens.java
+++ src/main/java/com/example/IncheonMate/common/auth/dto/Tokens.java
@@ -3,4 +3,7 @@ package com.example.IncheonMate.common.auth.dto;
 public record Tokens(
         String accessToken, String refreshToken, String role
 ) {
+    public static Tokens of(String accessToken,String refreshToken,String role){
+        return new Tokens(accessToken,refreshToken,role);
+    }
 }
