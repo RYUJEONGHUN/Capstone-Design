e23a7925053a7c8e1ac483d58a2847e2ecb3e84f
김재원 <wodnjs9058@naver.com>
2026-03-21 20:35:01 +0900

    feat: 오늘한 채팅 내역을 반환하는 /chat/today를 위한 메서드 정의

--- src/main/java/com/example/IncheonMate/chat/repository/ChatSessionRepository.java
+++ src/main/java/com/example/IncheonMate/chat/repository/ChatSessionRepository.java
@@ -2,7 +2,9 @@ package com.example.IncheonMate.chat.repository;
 
 import com.example.IncheonMate.chat.domain.ChatSession;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +15,10 @@ public interface ChatSessionRepository extends MongoRepository<ChatSession, Stri
 
     //채팅 키워드 검색할 때 사용(searchMessagesByKeyword)
     List<ChatSession> findByMemberIdAndMessagesContentContaining(String id, String keyword);
+
+    //오늘 채팅한 기록을 찾을때
+    // memberId가 일치하고, 생성일과 마지막 메시지일이 start(오늘 00시)와 end(오늘 23시59분) 사이인 세션 조회
+    @Query("{ 'memberId': ?0, 'createdAt': { $gte: ?1, $lte: ?2 } }")
+    Optional<ChatSession> findTodaySessionByMemberId(String memberid, LocalDateTime start, LocalDateTime end);
+
 }
