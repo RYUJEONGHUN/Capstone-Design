6164eb21eb66a7122bf880bff86ad5729ce58ab4
김재원 <wodnjs9058@naver.com>
2026-05-07 22:36:21 +0900

    feat: 회원가입시 게스트 채팅 내역 Mongo DB로 마이그레이션

--- src/main/java/com/example/IncheonMate/member/service/OnboardingService.java
+++ src/main/java/com/example/IncheonMate/member/service/OnboardingService.java
@@ -1,5 +1,9 @@
 package com.example.IncheonMate.member.service;
 
+import com.example.IncheonMate.chat.domain.ChatSession;
+import com.example.IncheonMate.chat.domain.GuestChatSession;
+import com.example.IncheonMate.chat.repository.ChatSessionRepository;
+import com.example.IncheonMate.chat.repository.GuestChatSessionRepository;
 import com.example.IncheonMate.common.auth.dto.CustomOAuth2User;
 import com.example.IncheonMate.common.exception.CustomException;
 import com.example.IncheonMate.common.exception.ErrorCode;
@@ -19,10 +23,15 @@ import org.jspecify.annotations.Nullable;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -32,6 +41,8 @@ public class OnboardingService {
     private final MemberRepository memberRepository;
     private final MemberCommonService memberCommonService;
     private final StringRedisTemplate redisTemplate;
+    private final GuestChatSessionRepository guestChatSessionRepository;
+    private final ChatSessionRepository chatSessionRepository;
     //현재 약관 버전
     private static final String CURRENT_TERMS_VERSION = "v1.0.0";
 
@@ -42,12 +53,12 @@ public class OnboardingService {
 
         log.info("'{}' 사상의학 테스트 결과: {}", email, sasangType);
 
-        return new MemberCommonDto.SasangResponseDto(email,sasangType);
+        return new MemberCommonDto.SasangResponseDto(email, sasangType);
     }
 
     //초기 입력 화면(온보딩)에서 사용자가 입력한 정보 전체의 정책을 검사하고 저장하는 서비스
     @Transactional
-    public void saveOnboarding(String email, OnboardingBundle.OnboardingDto onboardingDto,String guestId,String provider,String name) {
+    public void saveOnboarding(String email, OnboardingBundle.OnboardingDto onboardingDto, String guestId, String provider, String name) {
         /*
         String nickname -> 최소 2글자/'사용자' 미포함
         String birthdate -> 6자리 숫자
@@ -62,12 +73,12 @@ public class OnboardingService {
         //온보딩DTO null 검증
         if (onboardingDto == null) {
             log.warn("[Member] 온보딩 데이터가 Null임");
-            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE,"저장할 온보딩 데이터가 업습니다.");
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE, "저장할 온보딩 데이터가 업습니다.");
         }
 
         //닉네임이 정책에 맞게 들어왔는지 검증
         if (!memberCommonService.checkNicknamePolicy(onboardingDto.nickname())) {
-            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE,onboardingDto.nickname() + "은(는) 정책을 위배한 닉네임입니다");
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE, onboardingDto.nickname() + "은(는) 정책을 위배한 닉네임입니다");
         }
         //yyMMdd형식을 yyyy-MM-dd형식으로 변환하고 미래의 날짜인지 검증
         LocalDate birthDate = memberCommonService.parseLocalDate(onboardingDto.birthDate());
@@ -98,20 +109,75 @@ public class OnboardingService {
         memberRepository.save(newMember);
 
         //게스트 계정으로 가입한 내역이 있는 멤버이면 채팅 내역도 DB에 저장해야함
-        if(!"newUser".equals(guestId)){
-            //---------------------나중에 채팅 기능 추가하면 코드 작성---------------------
-            //1. GUEST_CHAT DB로 옮기기 -> 2. save하기 -> 3.Redis에서 GUEST_CHAT과 GUEST_COUNT 삭제하기
-
+        if (!"newUser".equals(guestId)) {
+            if (!migrateGuestChatHistory(newMember.getId(), guestId)) {
+                log.warn("[Member] 게스트 채팅 내역 마이그레이션 실패 (GuestId: {})", guestId);
+                throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR, "게스트 채팅 내역 마이그레이션 실패");
+            }
             //GUEST_RROFILE 삭제하기
-            redisTemplate.delete("GUEST_PROFILE:"+guestId);
+            redisTemplate.delete("GUEST_PROFILE:" + guestId);
             //ROLE_GUEST로 만든 Refresh Token도 Redis에서 삭제하기
-            redisTemplate.delete("RT:"+guestId);
-            log.info("[Member] 게스트 Redis 데이터 정리 완료 (GuestId: {})",guestId);
+            redisTemplate.delete("RT:" + guestId);
+            log.info("[Member] 게스트 Redis 데이터 정리 완료 (GuestId: {})", guestId);
         }
 
         log.info("[Member] 정회원 가입 완료");
     }
 
+    @Transactional
+    public boolean migrateGuestChatHistory(String memberId, String guestId) {
+        try {
+            //1. NPE 방어
+            if (memberId == null || guestId == null) {
+                log.warn("[Member] [Migration] memberId 또는 guestId가 null입니다.");
+                return false;
+            }
+            //2. redis repository에서 가져오기
+            Optional<GuestChatSession> guestChatSessionOpt = guestChatSessionRepository.findById(guestId);
+            if (guestChatSessionOpt.isEmpty()) {
+                log.warn("[Member] [Migration] 마이그레이션할 게스트 채팅 내역이 없음 (GuestId: {})", guestId);
+                return false;
+            }
+            GuestChatSession guestChatSession = guestChatSessionOpt.get();
+
+            //3. 이미 이전완료 되었는지 검사
+            if (chatSessionRepository.existsById(guestChatSession.getId())) {
+                log.info("[Member] [Migration] 이미 이전된 채팅 세션입니다. (SessionId: {})", guestChatSession.getId());
+                return true;
+            }
+
+            // 4. Messages 마이그레이션
+            List<ChatSession.Message> memberMessageList = new ArrayList<>();
+            if (!CollectionUtils.isEmpty(guestChatSession.getMessages())) {
+                memberMessageList = guestChatSession.getMessages().stream()
+                        .map(guestMessage -> ChatSession.Message.builder()
+                                .id(guestMessage.getId())
+                                .messagedAt(guestMessage.getMessagedAt())
+                                .authorType(guestMessage.getAuthorType())
+                                .content(guestMessage.getContent())
+                                .chatResponseProvider(guestMessage.getChatResponseProvider())
+                                .chatResponseType(guestMessage.getChatResponseType())
+                                .build())
+                        .collect(Collectors.toList());
+            }
+
+            //5. ChatSession 마이그레이션
+            ChatSession memberChatSession = ChatSession.builder()
+                    .id(guestChatSession.getId())
+                    .title(guestChatSession.getTitle())
+                    .createdAt(guestChatSession.getCreatedAt())
+                    .lastMessageAt(guestChatSession.getLastMessageAt())
+                    .memberId(memberId)
+                    .messages(memberMessageList)
+                    .build();
+
+            chatSessionRepository.save(memberChatSession);
+            return true;
+        } catch (Exception e) {
+            log.error("[Member] [Migration] 채팅 내역 이전 중 시스템 오류 발생. (GuestId: {})", guestId, e);
+            return false;
+        }
+    }
 
 
 //    //saveAgreements컨트롤러
@@ -138,10 +204,10 @@ public class OnboardingService {
 //        return OnboardingBundle.TermsAgreementResponse.from(updatedMember);
 //    }
 
-    //getOnboardingData 서비스
-    public OnboardingBundle.OnboardingDto getOnboardingValues(String email) {
-        OnboardingBundle.OnboardingDto result =  OnboardingBundle.OnboardingDto.from(memberRepository.findByEmailOrElseThrow(email));
-        log.info("[Member] 사용자 정보 조회 성공");
-        return result;
+        //getOnboardingData 서비스
+        public OnboardingBundle.OnboardingDto getOnboardingValues (String email){
+            OnboardingBundle.OnboardingDto result = OnboardingBundle.OnboardingDto.from(memberRepository.findByEmailOrElseThrow(email));
+            log.info("[Member] 사용자 정보 조회 성공");
+            return result;
+        }
     }
-}
